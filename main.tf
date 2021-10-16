fbb5f2a4c9061f62b62b31f8c47ca4d039d627f4
czeglediptr <czegledi.ptr@gmail.com>
2021-10-16 12:00:07 +0200

    tf added r7

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
 resource "local_file" "inv" {
-  content  = templatefile("${path.module}/inv.tpl", {ip_addr = "192.168.123.4"})
+  content  = templatefile("${path.module}/inv.tpl", {ip_addr = "localhost"})
   filename = "inv"
 }
