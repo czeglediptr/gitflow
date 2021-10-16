58a8303a194b249a3701e52ce3dc9636a5e5767b
czeglediptr <czegledi.ptr@gmail.com>
2021-10-16 11:41:08 +0200

    tf added r5

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
 resource "local_file" "inv" {
-  content  = templatefile("${path.module}/backends.tpl", {ip_addr = "10.0.0.5"})
+  content  = templatefile("${path.module}/inv.tpl", {ip_addr = "10.0.0.5"})
   filename = "inv"
 }
