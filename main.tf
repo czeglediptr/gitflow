fe007be6d9b783cb25c911dc5e25411a28cf442a
czeglediptr <56045238+czeglediptr@users.noreply.github.com>
2021-10-21 16:00:51 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,3 +2,13 @@ resource "local_file" "inv" {
   content  = templatefile("${path.module}/inv.tpl", {ip_addr = ["localhost", "1.2.3.4", "4.5.6.7", "4.5.6.7", "4.6.6.7", "4.5.8.7", "4.5.60.7", "4.5.62.7", "4.15.6.7", "4.35.6.7"]})
   filename = "inv"
 }
+
+resource "null_resource "bimbam" {
+  whatatrributeisit = "neeeeem"
+}
+
+resource "null_resource "bimbum" {}
+resource "null_resource "bimbom" {}
+resource "null_resource "bimbem" {}
+resource "null_resource "bimbm" {}
+resource "null_resource "bimbffam" {}
