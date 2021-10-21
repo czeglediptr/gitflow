e08085de2e68d9384b97adbdc02f2f6b3cb0bfc0
czeglediptr <56045238+czeglediptr@users.noreply.github.com>
2021-10-21 15:40:24 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
 resource "local_file" "inv" {
-  content  = templatefile("${path.module}/inv.tpl", {ip_addr = "localhost"})
+  content  = templatefile("${path.module}/inv.tpl", {ip_addr = ["localhost", "1.2.3.4", "4.5.6.7", "4.5.6.7", "4.6.6.7", "4.5.8.7", "4.5.60.7", "4.5.62.7", "4.15.6.7", "4.35.6.7"]})
   filename = "inv"
 }
