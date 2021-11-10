90e02b153c686d3c884751314eca79dd7ccfb704
czeglediptr <56045238+czeglediptr@users.noreply.github.com>
2021-11-10 20:33:39 +0100

    Update parse_json.js

--- parse_json.js
+++ parse_json.js
@@ -16,19 +16,19 @@ exec("terraform state pull", (error, stdout, stderr) => {
 //let jsonData = require('./test.json');
 
 
-for (const [cucc, value] of Object.entries(jsonData)) {
-  if(cucc == "resources") {
-    for (const x of jsonData[cucc]) {
-        if(x["name"] == "inv") {
-            for (const y of x["instances"]) {
-                if(y["attributes"]["content"]) {
-                    console.log(y["attributes"]["content"])
-                }
+// for (const [cucc, value] of Object.entries(jsonData)) {
+//   if(cucc == "resources") {
+//     for (const x of jsonData[cucc]) {
+//         if(x["name"] == "inv") {
+//             for (const y of x["instances"]) {
+//                 if(y["attributes"]["content"]) {
+//                     console.log(y["attributes"]["content"])
+//                 }
                 
-            }
-        }
-    }
-  }
-}
+//             }
+//         }
+//     }
+//   }
+// }
 
 //console.log(jsonData.resources[0].instances[0].attributes.content);
