9ba6be00ec92e4c92d9d9993d81ba2e073159134
kolopka <czegledi.ptr@gmail.com>
2021-11-20 11:29:30 +0100

    test '>' with stdout redirection

--- parse_json.js
+++ parse_json.js
@@ -1,34 +1,34 @@
 'use strict';
-const { exec } = require("child_process");
+// const { exec } = require("child_process");
 
-exec("terraform state pull", (error, stdout, stderr) => {
-    if (error) {
-        console.log(`error: ${error.message}`);
-        return;
-    }
-    if (stderr) {
-        console.log(`stderr: ${stderr}`);
-        return;
-    }
-    console.log(`stdout: ${stdout}`);
-});
-
-//let jsonData = require('./test.json');
-
-
-// for (const [cucc, value] of Object.entries(jsonData)) {
-//   if(cucc == "resources") {
-//     for (const x of jsonData[cucc]) {
-//         if(x["name"] == "inv") {
-//             for (const y of x["instances"]) {
-//                 if(y["attributes"]["content"]) {
-//                     console.log(y["attributes"]["content"])
-//                 }
-                
-//             }
-//         }
+// exec("terraform state pull", (error, stdout, stderr) => {
+//     if (error) {
+//         console.log(`error: ${error.message}`);
+//         return;
 //     }
-//   }
-// }
+//     if (stderr) {
+//         console.log(`stderr: ${stderr}`);
+//         return;
+//     }
+//     console.log(`stdout: ${stdout}`);
+// });
 
-//console.log(jsonData.resources[0].instances[0].attributes.content)
+let jsonData = require('./test.json');
+
+
+for (const [cucc, value] of Object.entries(jsonData)) {
+  if(cucc == "resources") {
+    for (const x of jsonData[cucc]) {
+        if(x["name"] == "inv") {
+            for (const y of x["instances"]) {
+                if(y["attributes"]["content"]) {
+                    console.log(y["attributes"]["content"])
+                }
+                
+            }
+        }
+    }
+  }
+}
+
+console.log(jsonData.resources[0].instances[0].attributes.content)
